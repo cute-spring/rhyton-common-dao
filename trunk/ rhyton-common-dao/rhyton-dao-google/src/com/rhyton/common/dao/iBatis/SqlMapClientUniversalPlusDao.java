9aab95d38468ae3f88c3b34736929230f8315d9f
cute.spring <cute.spring@f396dbf2-ae41-11dd-8e2c-133076370c3f>
2009-01-05 05:57:01 +0000

    暂时不打算使用

--- trunk/ rhyton-common-dao/rhyton-dao-google/src/com/rhyton/common/dao/iBatis/SqlMapClientUniversalPlusDao.java
+++ trunk/ rhyton-common-dao/rhyton-dao-google/src/com/rhyton/common/dao/iBatis/SqlMapClientUniversalPlusDao.java
@@ -2,6 +2,23 @@ package com.rhyton.common.dao.iBatis;
 
 import com.rhyton.common.dao.UniversalDao;
 @Deprecated
-public interface SqlMapClientUniversalPlusDao extends UniversalDao{
+public interface SqlMapClientUniversalPlusDao extends UniversalDao {
+	// <T> PaginationHolder<T> getPaginationHolder(PaginationInfo pageInfo, T exampleEntity);
+	//
+	// <T> PaginationHolder<T> getPaginationHolder(String daoMethodName, PaginationInfo pageInfo, T exampleEntity);
+	//
+	// <T> List<T> queryByExample(T exampleEntity) throws DataAccessException;
+	//
+	// <T> T queryForObject(T exampleEntity);
+	//    
+	//<T> T queryForObject(Class<T> clazz, String daoMethodName) throws DataAccessException;
+
+	//<T> T queryForObject(Class<T> clazz, String daoMethodName, Object parameterObject) throws DataAccessException;
+	//
+	// <T> List<T> queryForList(Class<T> clazz,String daoMethodName) throws DataAccessException;
+	//
+	// <T> List<T> queryForList(Class<T> clazz,String daoMethodName, Object parameterObject) throws DataAccessException;
+	//
+	// <T> void batchUpdate(List<T> parameterList) throws DataAccessException;
 
 }
--- trunk/ rhyton-common-dao/rhyton-dao-google/src/com/rhyton/common/dao/iBatis/SqlMapClientUniversalPlusDaoImp.java
+++ trunk/ rhyton-common-dao/rhyton-dao-google/src/com/rhyton/common/dao/iBatis/SqlMapClientUniversalPlusDaoImp.java
@@ -3,19 +3,19 @@ package com.rhyton.common.dao.iBatis;
 import java.io.Serializable;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.orm.ObjectRetrievalFailureException;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 import org.springframework.util.ClassUtils;
 
-import com.rhyton.common.dao.UniversalDao;
 import com.rhyton.common.pagination.domain.PaginationHolder;
 import com.rhyton.common.pagination.domain.PaginationInfo;
 import com.rhyton.common.pagination.ibatis.IbatisPaginationFactory;
 @Deprecated
-public class SqlMapClientUniversalPlusDaoImp extends SqlMapClientDaoSupport implements UniversalDao {
+@SuppressWarnings("unchecked")
+public class SqlMapClientUniversalPlusDaoImp extends SqlMapClientDaoSupport implements SqlMapClientUniversalPlusDao {
 	/**
 	 * Log variable for all child classes. Uses LogFactory.getLog(getClass()) from Commons Logging
 	 */
@@ -24,7 +24,7 @@ public class SqlMapClientUniversalPlusDaoImp extends SqlMapClientDaoSupport impl
 	/**
 	 * {@inheritDoc}
 	 */
-	@SuppressWarnings("unchecked")
+
 	public <T> List<T> getAll(Class<T> clazz) {
 		return getSqlMapClientTemplate().queryForList(iBatisDaoUtils.getSelectQuery(ClassUtils.getShortName(clazz)), null);
 	}
@@ -33,7 +33,6 @@ public class SqlMapClientUniversalPlusDaoImp extends SqlMapClientDaoSupport impl
 	 * {@inheritDoc}
 	 */
 
-	@SuppressWarnings("unchecked")
 	public <T> T get(Class<T> clazz, Serializable primaryKey) {
 		Object object = getSqlMapClientTemplate().queryForObject(iBatisDaoUtils.getFindQuery(ClassUtils.getShortName(clazz)),
 				primaryKey);
@@ -47,7 +46,7 @@ public class SqlMapClientUniversalPlusDaoImp extends SqlMapClientDaoSupport impl
 	 * {@inheritDoc}
 	 */
 	public <T> T save(final T object) {
-	 
+
 		String className = object.getClass().getName();
 		Object primaryKey = iBatisDaoUtils.getPrimaryKeyValue(object);
 		// Class primaryKeyClass = iBatisDaoUtils.getPrimaryKeyField(object).getType();
@@ -59,8 +58,8 @@ public class SqlMapClientUniversalPlusDaoImp extends SqlMapClientDaoSupport impl
 		}
 
 		// check for new record
-		if (StringUtils.isBlank(keyId)) {
-			primaryKey = getSqlMapClientTemplate().insert(iBatisDaoUtils.getInsertQuery(className), object);		 
+		if (org.apache.commons.lang.StringUtils.isBlank(keyId)) {
+			primaryKey = getSqlMapClientTemplate().insert(iBatisDaoUtils.getInsertQuery(className), object);
 		} else {
 			getSqlMapClientTemplate().update(iBatisDaoUtils.getUpdateQuery(className), object);
 		}
@@ -72,7 +71,7 @@ public class SqlMapClientUniversalPlusDaoImp extends SqlMapClientDaoSupport impl
 			return object;
 		}
 	}
- 
+
 	public <T> PaginationHolder<T> getPaginationHolder(PaginationInfo pageInfo, T exampleEntity) {
 		return new IbatisPaginationFactory<T>(getSqlMapClientTemplate(), exampleEntity).getPaginationHolder(pageInfo);
 	}
@@ -81,6 +80,15 @@ public class SqlMapClientUniversalPlusDaoImp extends SqlMapClientDaoSupport impl
 		getSqlMapClientTemplate().update(iBatisDaoUtils.getDeleteByPrimaryKeyQuery(clazz.getName()), id);
 	}
 
- 
+	public <T> T queryForObject(Class<T> clazz, String daoMethodName) throws DataAccessException {
+		return (T) getSqlMapClientTemplate().queryForObject(getStatementName(clazz, daoMethodName));
+	}
 
+	public <T> T queryForObject(Class<T> clazz, String daoMethodName, Object parameterObject) throws DataAccessException {
+		return (T) getSqlMapClientTemplate().queryForObject(getStatementName(clazz, daoMethodName), parameterObject);
+	}
+
+	private final String getStatementName(Class clazz, String methodName) {
+		return clazz.getName() + "." + methodName;
+	}
 }
